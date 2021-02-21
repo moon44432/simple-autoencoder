c21e235eae6b46f9a10cb2e6b06ae83b99fed6a2
Moon Seung-gi <moon44432@naver.com>
2021-02-21 22:09:44 +0900

    Conv & TransposedConv

--- simple_ae.py
+++ simple_ae.py
@@ -4,73 +4,95 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import tensorflow as tf
 
-from tensorflow.keras.layers import Input, Dense, Conv2D, Conv2DTranspose, LeakyReLU, Flatten
-from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, \
+    Conv2DTranspose, LeakyReLU, Flatten, Dropout, BatchNormalization, Reshape
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras import backend as K
 from tensorflow.keras import metrics
+from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras.datasets import mnist
 import numpy as np
 
 # hparams
 batch_size = 64
-original_dim = 28 ** 2
+original_dim = [28, 28, 1]
 latent_dim = 2
-epochs = 50
+epochs = 20
 epsilon_std = 1.0
 
 
 def create_encoder():
-    x = Input(shape=(original_dim,), name='input')
-    c1 = Conv2D(32, (5, 5), strides=2, padding='same')(x)
-    c1 = LeakyReLU(c1)
-    c2 = Conv2D(64, (5, 5), strides=2, padding='same')(c1)
-    c2 = LeakyReLU(c2)
-    f = Flatten(c2)
-    z = Dense(latent_dim, name='latent')(f)
-    encoder = Model(x, z, name='encoder')
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), strides=(2, 2), padding='same',
+                            input_shape=original_dim))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.3))
 
-    encoder.summary()
-    return encoder
+    model.add(Conv2D(64, (5, 5), strides=(2, 2), padding='same'))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(64, (5, 5), strides=(1, 1), padding='same'))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.3))
+
+    model.add(Flatten())
+    model.add(Dense(latent_dim))
+
+    model.summary()
+    return model
 
 
 def create_decoder():
-    decoder_input = Input(shape=(latent_dim,), name='decoder_input')
-    decoder_h1 = Dense(16, activation='relu', name='decoder_h1')(decoder_input)
-    decoder_h2 = Dense(256, activation='relu', name='decoder_h2')(decoder_h1)
-    x_decoded = Dense(original_dim, activation='sigmoid', name='flat_decoded')(decoder_h2)
-    decoder = Model(decoder_input, x_decoded, name='decoder')
+    model = tf.keras.Sequential()
+    model.add(Dense(7 * 7 * 64, use_bias=False, input_shape=(latent_dim,)))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
 
-    decoder.summary()
-    return decoder
+    model.add(Reshape((7, 7, 64)))
+    assert model.output_shape == (None, 7, 7, 64)
+
+    model.add(Conv2DTranspose(64, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 64)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(64, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 64)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 32)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    model.summary()
+    return model
 
 
-def create_network():
-    x = Input(shape=(original_dim,), name='input')
+if __name__ == '__main__':
+    x = Input(shape=original_dim, name='input')
 
     encoder = create_encoder()
     decoder = create_decoder()
 
     output = decoder(encoder(x))
     ae = Model(x, output)
-
-    return ae
-
-
-if __name__ == '__main__':
-    ae = create_network()
     ae.compile(optimizer='rmsprop', loss='binary_crossentropy')
 
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train.astype('float32') / 255.
     x_test = x_test.astype('float32') / 255.
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+    x_train = x_train.reshape((len(x_train), 28, 28, 1))
+    x_test = x_test.reshape((len(x_test), 28, 28, 1))
 
     ae.fit(x_train, x_train, shuffle=True, epochs=epochs, batch_size=batch_size)
-
-    encoder = create_encoder()
-    decoder = create_decoder()
+    ae.save('model.h5')
 
     x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
     plt.figure(figsize=(6, 6))
@@ -79,12 +101,10 @@ if __name__ == '__main__':
     plt.savefig('latent_space.png')
     plt.show()
 
-    # 숫자의 2D 매니폴드 출력하기
-    n = 15  # 15x15 숫자를 담은 그림
+    n = 15
     digit_size = 28
     figure = np.zeros((digit_size * n, digit_size * n))
-    # 잠재 공간을 가우스 분포로 가정했기 때문에 잠재 변수 z의 값을 만들기 위해
-    # 일정 간격으로 떨어진 좌표를 가우스 분포의 역 CDF(ppf)를 통해 변환합니다.
+
     grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
     grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
